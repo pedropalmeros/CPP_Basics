bb16f13fbb272ec35820aecd5abf16ab14cae1d1
Palmeros <pedrofpalmeros@gmail.com>
2023-12-17 17:38:59 -0600

    Adding sorting algorithm to the practice 02

--- Unit_03_Arrays/02_Array_Practice/02_Array_Practice.cpp
+++ Unit_03_Arrays/02_Array_Practice/02_Array_Practice.cpp
@@ -2,6 +2,8 @@
 
 using namespace std; 
 
+int getMin(int a, int b);
+
 int main(){
     int userInput[40];
 
@@ -22,8 +24,38 @@ int main(){
 
     // printing the array in reverse order
     cout << "Printing the array in reverser order: \n\n";
-    for(int i = 40; i > 0 ; --i){
+    for(int i = 39; i >= 0 ; i--){
         cout << userInput[i];
         cout << " ";
+    }
+
+    // SORTING algorithms
+    for (int i = 0; i < 40; i++){
+        for (int j = 0; j < 39 - i; j++){
+            if(userInput[i] > userInput[j+1]){
+                int temp; 
+                temp = userInput[i]; 
+                userInput[i] = userInput[j+1]; 
+                userInput[j+1] = temp;
+            }
+        }
+    }
+
+    // PRINTING THE SORTED ARRAY
+    cout << "Printing the array: \n\n";
+    for(int i = 0; i < 40 ; i++){
+        cout << userInput[i];
+        cout << " ";
+    }
+
+
+}
+
+int getMin(int a, int b){
+    if (a <= b){
+        return a;
+    }
+    else{
+        return b; 
     }
 }
